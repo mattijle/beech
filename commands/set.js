6f765ac1dda91e8cb932767a94203a876111ce6b
Matti Leppänen <matti@mattijle.com>
2021-10-13 15:39:24 +0300

    adds options to set channel for the bot to operate in

--- commands/set.js
+++ commands/set.js
@@ -1,26 +1,29 @@
 const { SlashCommandBuilder, codeBlock } = require('@discordjs/builders');
-const { updateGuildRoles } = require('../db');
+const { updateGuildRoles, updateGuildInfoChannel } = require('../db');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('set')
         .setDescription('Muuta botin palvelin kohtaisia asetuksia.')
-        .addStringOption(option => option.setName('roolit').setDescription('Aseta botin tarjoamat roolit. (huom. ei luo uusia rooleja.)')),
-    //.addStringOption(option => option.setName('kanava').setDescription('Kanava(t) joilla haluat botin viestivän.'))
+        .addStringOption(option => option.setName('roolit').setDescription('Aseta botin tarjoamat roolit. (huom. ei luo uusia rooleja.)'))
+        .addStringOption(option => option.setName('kanava').setDescription('Aseta botin käyttämä kanava')),
+    //.addStringOption(option => option.setName('kanava').setDescription('Kanava joilla haluat botin viestivän.'))
     async execute(interaction) {
         if (!interaction.memberPermissions.has('0x00000008', true)) {
             await interaction.reply({ content: 'Tarvitset admin oikeudet', ephemeral: true });
             return;
         }
-        const roles = interaction.options.getString('roolit').split(',').trim();
-        //const channels = interaction.option.getString('kanava');
+        const optsRoles = interaction.options.getString('roolit')
 
-        if (!roles && !kanava) {
+        const optsChannels = interaction.options.getString('kanava');
+
+        if (!optsRoles && !optsChannels) {
             await interaction.reply({ content: 'Mitään ei muutettu', ephemeral: true });
             return;
         }
         const guild = interaction.guild;
         let reply = '';
-        if (roles) {
+        if (optsRoles) {
+            const roles = optsRoles.split(',').trim();
             const guildroles = await guild.roles.fetch();
             const addedRoles = roles.reduce((prev, role) => {
                 gRole = guildroles.find((r => r.name === role));
@@ -31,6 +34,12 @@ module.exports = {
             }, []);
             updateGuildRoles(guild.name, addedRoles);
             reply += `Pyydettävät roolit: ${addedRoles.toString()}`
+        }
+        if (optsChannels) {
+            const channelId = optsChannels.replace(/<|#|>/g, '')
+            const channel = await guild.channels.fetch(channelId)
+            updateGuildInfoChannel(guild.name, channelId);
+            reply += `\n Uusi info kanava asetettu: #${channel.name}`
         }
         await interaction.reply({ content: codeBlock('js', reply), ephemeral: true });
     },
