570cb604bd652582f92c96377831a747fb44b3a8
Matti Leppänen <matti@mattijle.com>
2021-10-13 15:38:28 +0300

    adds safetycheck for infoChannelId

--- events/ready.js
+++ events/ready.js
@@ -13,29 +13,33 @@ module.exports = {
 		const guilds = await client.guilds.fetch()
 		guilds.each(guild => register(guild.id))
 
+		// Tämä ratkaisu ei toimi jos botti on useammalla palvelimella. 
+		// Eikä tarjoa mahdollisuutta vaihtaa kanavaa botin ollessa käynnissä.
+
+		if (!infoChannelId) return
 		const infoChannel = await client.channels.fetch(infoChannelId)
 		if (!infoChannel) return
 		infoChannel.messages.fetch(roleMessageId)
-		.then(msg => {
-			const filter = (reaction) => {
-				return ['😄','\:smile:',].includes(reaction.emoji.name)
-			}
+			.then(msg => {
+				const filter = (reaction) => {
+					return ['😄', '\:smile:',].includes(reaction.emoji.name)
+				}
 
-			const reactionCollector = msg.createReactionCollector({filter, dispose: true})
+				const reactionCollector = msg.createReactionCollector({ filter, dispose: true })
 
-			reactionCollector.on('collect', (reaction, user) => {
-				console.log(`Reaction user: ${user} didsomething? emoji:${reaction.emoji.name}`)
+				reactionCollector.on('collect', (reaction, user) => {
+					console.log(`Reaction user: ${user} didsomething? emoji:${reaction.emoji.name}`)
+				})
+				reactionCollector.on('create', (reaction, user) => {
+					console.log(`Reaction user: ${user} created emoji:${reaction.emoji.name}`)
+				})
+				reactionCollector.on('remove', (reaction, user) => {
+					console.log(`Reaction user: ${user} removed the emoji:${reaction.emoji.name}`)
+				})
+				reactionCollector.on('dispose', (reaction, user) => {
+					console.log(`Reaction user: ${user} disposed the emoji:${reaction.emoji.name}`)
+				})
 			})
-			reactionCollector.on('create', (reaction, user) => {
-				console.log(`Reaction user: ${user} created emoji:${reaction.emoji.name}`)
-			})
-			reactionCollector.on('remove', (reaction, user) => {
-				console.log(`Reaction user: ${user} removed the emoji:${reaction.emoji.name}`)
-			})
-			reactionCollector.on('dispose', (reaction, user) => {
-				console.log(`Reaction user: ${user} disposed the emoji:${reaction.emoji.name}`)
-			})
-		})	
 	}
 }
 
