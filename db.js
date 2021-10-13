85690a27d8c67a18a8f13373c0f24cdd3f9f086e
Matti Leppänen <matti@mattijle.com>
2021-10-13 15:45:06 +0300

    adds functions to get and update guild rolemessageid and infochannelid

--- db.js
+++ db.js
@@ -7,7 +7,7 @@ var instance = new KeyValue({
 instance.load('./data/data.yaml');
 
 const save = () => {
-    instance.save('./data/data.yaml', {
+    return instance.save('./data/data.yaml', {
         format: 'yaml',
         indent: 4,
         atomic: true
@@ -17,7 +17,22 @@ exports.save = save;
 exports.updateGuildRoles = (guild, roles) => {
     instance.set(guild, 'roles', roles)
     save();
+    return;
 }
 exports.getGuildRoles = (guild) => {
     return instance.get(guild, 'roles');
+}
+exports.getGuildInfoChannel = (guild) => {
+    return instance.get(guild, 'channel');
+}
+exports.getGuildRoleMessage = (guild) => {
+    return instance.get(guild, 'RoleMessage');
+}
+exports.updateGuildRoleMessage = (guild, messageId) => {
+    instance.set(guild, 'RoleMessage', messageId);
+    return save();
+}
+exports.updateGuildInfoChannel = (guild, channelId) => {
+    instance.set(guild, 'channel', channelId);
+    return save();
 }
